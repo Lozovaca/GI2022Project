7126360f4e3292e0519fd4dbe05a5f37997ccd08
svelimirovic96 <svelimirovic96@gmail.com>
2022-04-28 22:15:19 +0200

    add bwm and fm index for index search in text

--- Burrows0Wheeler.py
+++ Burrows0Wheeler.py
@@ -11,12 +11,16 @@ def bwtViaBwm(t):
 ### Given T, returns BWT(T) by creating BWM """
     return ''.join(map(lambda x: x[-1], bwm(t)))
 
+def firstColumnBwm(t):
+### Given T, returns BWT(T) by creating BWM """
+    return ''.join(map(lambda x: x[0], bwm(t)))
+
 def suffixArray(s):
     """ Given T return suffix array SA(T). We use Python's sorted
     function here for simplicity, but we can do better. """
     satups = sorted([(s[i:], i) for i in range(len(s))])
     # Extract and return just the offsets
-    return map(lambda x: x[1], satups)
+    return map(lambda x: (x[0],x[1]), satups)
 def bwtViaSa(t):
     """ Given T, returns BWT(T) by way of the suffix array. """
     bw = []
@@ -57,3 +61,57 @@ def reverseBwt(bw):
         # jump to row that starts with c of same rank
         rowi = first[c][0] + ranks[rowi]
     return t
+    
+def c_matrix_insert(t):
+    firstCols=firstColumnBwm(t)
+    c_matrix={}
+    for i in range(len(firstCols)-1):
+        if i==0: c_matrix[firstCols[i]]=i
+        if firstCols[i-1]!=firstCols[i]:  c_matrix[firstCols[i]]=i
+    return c_matrix 
+
+def occ_matrix_insert(t):
+    firstCols=firstColumnBwm(t) #first column
+    bwt=bwtViaBwm(t)    #last column
+    #occ=[[0 for _ in range(len(bwt))] for _ in range(len(firstCols))]
+    occ={}
+    firstCols=sorted(set(bwt))
+    for nt in firstCols:
+        occ[nt]=[0]*len(bwt)
+    for j in range(len(firstCols)):
+        val=0
+        for i in range(len(bwt)):
+            if firstCols[j]==bwt[i]: 
+                val+=1
+            occ[firstCols[j]][i]=val
+    return occ
+
+def bwm_search(t, query):
+    c=c_matrix_insert(t)  
+    occ=occ_matrix_insert(t)
+    p=query[::-1]
+    start=0
+    end=0
+    for i in range(len(p)):
+        character_c=p[i]
+        if i==0:
+            start=c[character_c]+1
+            firstCols=firstColumnBwm(t)
+            character_c_next=''
+            for j in range(len(firstCols)):
+                if firstCols[j-1]==character_c:
+                    character_c_next=firstCols[j]
+            end=c[character_c_next]
+        else:
+            start=c[character_c]+occ[character_c][start-1-1]+1
+            end=c[character_c]+occ[character_c][end-1]
+        #print([start,end])
+    return [start,end]
+
+t='BANANA$'
+p='ANA'
+#print(bwtViaSa(t))
+#print(list(suffixArray(t)))
+#print(c_matrix_insert(t))  
+#print(occ_matrix_insert(t))
+print(bwm_search(t,p))
